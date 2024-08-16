4bfc55af15bbbb64cf242f7f94761d46d9e6a5d1
mehrath <mehrath@hipergames.com>
2024-08-15 18:52:22 -0700

    Added new image

--- main.py
+++ main.py
@@ -12,32 +12,40 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 # Load image
-image = pygame.image.load('gfg.png')
-
+# image = pygame.image.load('gfg.png')
+boid = pygame.image.load('arrow.png')
 # Set the size for the image
-DEFAULT_IMAGE_SIZE = (200, 200)
+DEFAULT_IMAGE_SIZE = (20, 20)
 
 # Rotate the image by any degree
-image = pygame.transform.rotate(image, 180)
+# image = pygame.transform.rotate(image, 180)
+boid = pygame.transform.rotate(boid, 180)
 
 # Set a default position
-DEFAULT_IMAGE_POSITION = (200, 200)
+DEFAULT_IMAGE_POSITION = (20, 20)
 
 # Prepare loop condition
 running = True
 angle = 0
-
 # Event loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    is_key_pressed = pygame.key.get_pressed()
 
-    screen.fill((0, 0, 0))
-    rota_image = pygame.transform.rotate(image, angle)
-    screen.blit(rota_image, (0, 0))
+    if is_key_pressed[pygame.K_RIGHT]:
+        print('right')
+        angle += 1
+    elif is_key_pressed[pygame.K_LEFT]:
+        print('left')
+        angle -= 1
+    screen.fill((128, 128, 128))
+    # rota_image = pygame.transform.rotate(image, angle)
+    rota_boid = pygame.transform.rotate(boid, angle)
+    # screen.blit(rota_image, (0, 0))
+    screen.blit(rota_boid, (0, 0))
     pygame.display.update()
-    angle += 1
 
     # Part of event loop
     pygame.display.flip()
