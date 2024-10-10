062229e34d6620d2a32cad6a04f558fb9666b64e
mehrath <mehrath@hipergames.com>
2024-10-09 19:39:19 -0700

    Force of gravity for Earth added

--- physics.py
+++ physics.py
@@ -1,9 +1,29 @@
 import math
 
 from object import Object
+
 earthMass = 5.97219 * 10 ** 24
+# earthMass = 5.97219 * 10 ** 14
+objectMass = 1.0
+earthRadius = 6.374 * 10 ** 6
+objectRadius = 0.1
+TotalRadius = earthRadius + objectRadius
+TotalRadiusAskew = TotalRadius * TotalRadius
+otherMass = 1.0
 gravitationalConstant = 6.6743 * 10 ** -11
-def gravity(o1: Object, o2: Object) -> float:
+# gravitationalConstant = 6.6743 * 10 ** -1
+
+
+def gravity(m1: float, m2: float, radius: float) -> float:
+    g = ((m1 * m2) * gravitationalConstant) / (radius * radius)
+    return g
+
+
+def gravityObjects(o1: Object, o2: Object) -> float:
     radius = 10
     g = ((o1.m * o2.m) * 6.6743 * 10 ** -11) / (radius * radius)
     return g
+
+
+g = gravity(earthMass, objectMass, TotalRadius)
+print(g)
